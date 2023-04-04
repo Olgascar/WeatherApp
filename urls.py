e2e44e303159ce64869e27acbb2bfb6df7525c9c
Ольга Скарговская <119108613+Olgascar@users.noreply.github.com>
2023-04-04 17:11:42 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,8 +1,6 @@
-from django import urls
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('weather.urls'))
+    path('', views.index)
 ]
